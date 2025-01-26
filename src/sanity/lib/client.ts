bcc810d1333135fd684e61dbd5aac7593d534999
AmjadAfzalAhmed <abhutto2006@gmail.com>
2025-01-26 23:16:48 +0500

    ok

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -2,12 +2,11 @@ import { createClient } from 'next-sanity'
 
 import { apiVersion, dataset, projectId } from '../env'
 
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+const client = createClient({
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET, 
+  apiVersion: '2023-01-01', 
+});
 
 export const foodQuery = `*[_type == "food"]{
   _id,
